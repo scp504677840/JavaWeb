6444a1d1eb027b1e6783816ebe542268b32dde33
y1笑而过 <scp504677840@users.noreply.github.com>
2017-09-25 21:50:15 +0800

    使浏览器不缓存页面的过滤器
    使浏览器不缓存页面的过滤器

---
+++ Filter/NoCacheFilter/src/filter/NoCacheFilter.java
@@ -0,0 +1,19 @@
+package filter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class NoCacheFilter extends HttpFilter {
+
+    @Override
+    protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        response.setDateHeader("Expires", -1);
+        response.setHeader("Cache-Control", "no-cache");
+        response.setHeader("Pragma", "no-cache");
+        chain.doFilter(request,response);
+    }
+}
